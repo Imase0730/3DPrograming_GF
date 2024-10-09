131570dcc3914801efead26ef5be8d2d769e75e1
imase <imase0730@gmail.com>
2024-10-09 15:02:13 +0900

    キーボードとマウスの組み込み

--- Main.cpp
+++ Main.cpp
@@ -50,6 +50,12 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     if (FAILED(hr))
         return 1;
 
+    // キーボードの作成
+    std::unique_ptr<Keyboard> keyboard = std::make_unique<Keyboard>();
+
+    // マウスの作成
+    std::unique_ptr<Mouse> mouse = std::make_unique<Mouse>();
+
     g_game = std::make_unique<Game>();
 
     // Register class and create window
@@ -85,6 +91,9 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 
         if (!hwnd)
             return 1;
+
+        // マウスにウインドウハンドルを渡す
+        mouse->SetWindow(hwnd);
 
         ShowWindow(hwnd, nCmdShow);
         // TODO: Change nCmdShow to SW_SHOWMAXIMIZED to default to fullscreen.
@@ -213,6 +222,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
 
     case WM_ACTIVATEAPP:
+        
+        // 追加
+        Keyboard::ProcessMessage(message, wParam, lParam);
+        Mouse::ProcessMessage(message, wParam, lParam);
+
         if (game)
         {
             if (wParam)
@@ -280,12 +294,52 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
             s_fullscreen = !s_fullscreen;
         }
+        // 追加
+        Keyboard::ProcessMessage(message, wParam, lParam);
         break;
 
     case WM_MENUCHAR:
         // A menu is active and the user presses a key that does not correspond
         // to any mnemonic or accelerator key. Ignore so we don't produce an error beep.
         return MAKELRESULT(0, MNC_CLOSE);
+
+    case WM_ACTIVATE:
+//    case WM_ACTIVATEAPP:
+        Keyboard::ProcessMessage(message, wParam, lParam);
+        Mouse::ProcessMessage(message, wParam, lParam);
+        break;
+
+    //case WM_SYSKEYDOWN:
+    //    if (wParam == VK_RETURN && (lParam & 0x60000000) == 0x20000000)
+    //    {
+    //        // This is where you'd implement the classic ALT+ENTER hotkey for fullscreen toggle
+    //        ...
+    //    }
+    //    Keyboard::ProcessMessage(message, wParam, lParam);
+    //    break;
+
+    case WM_KEYDOWN:
+    case WM_KEYUP:
+    case WM_SYSKEYUP:
+        Keyboard::ProcessMessage(message, wParam, lParam);
+        break;
+
+//    case WM_ACTIVATE:
+//    case WM_ACTIVATEAPP:
+    case WM_INPUT:
+    case WM_MOUSEMOVE:
+    case WM_LBUTTONDOWN:
+    case WM_LBUTTONUP:
+    case WM_RBUTTONDOWN:
+    case WM_RBUTTONUP:
+    case WM_MBUTTONDOWN:
+    case WM_MBUTTONUP:
+    case WM_MOUSEWHEEL:
+    case WM_XBUTTONDOWN:
+    case WM_XBUTTONUP:
+    case WM_MOUSEHOVER:
+        Mouse::ProcessMessage(message, wParam, lParam);
+        break;
     }
 
     return DefWindowProc(hWnd, message, wParam, lParam);
